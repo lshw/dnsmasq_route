a20b070f37ee8d4106da7cfe8f77db61ce696fff
liu shiwei <liushiwei@gmail.com>
2021-12-30 10:05:19 +0800

    logread只过滤出dnsmasq的log

--- dnsmasq_route.sh
+++ dnsmasq_route.sh
@@ -4,4 +4,4 @@
 if ! [ "`ip route list default table 107 |grep 10.7.0.1`" ] ; then
  ip route add default table 107
 fi
-logread -f -S 128000 |dnsmasq_route -d 8.8.4.4 -r 10.7.0.1 -v -V -t 107
+logread -f -S 128000 -e dnsmasq |dnsmasq_route -d 8.8.4.4 -r 10.7.0.1 -v -V -t 107
