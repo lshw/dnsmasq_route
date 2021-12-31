ee3e93e1d1a46bef3471df82832f5acd25b1eb55
liu shiwei <liushiwei@gmail.com>
2021-12-31 09:10:08 +0800

    用 dnsmasq_route.sh 来启动

--- dnsmasq_route.sh
+++ dnsmasq_route.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-#多路由方式
-if ! [ "`ip route list default table 107 |grep 10.7.0.1`" ] ; then
- ip route add default table 107
+if [ $1 ] ; then
+ dns_server= $1
+else
+ dns_server= 10.7.0.1
 fi
-logread -f -S 128000 -e dnsmasq |dnsmasq_route -d 8.8.4.4 -r 10.7.0.1 -v -V -t 107
+
+if [ $2 ] ; then
+ remote_ip= $3
+else
+ remote_ip= 10.7.0.1
+fi
+
+if [ $3 ] ; then
+ table= $3
+else
+ table= 107
+fi
+
+#多路由方式
+if ! [ "`ip route list default table $table |grep $remote_ip`" ] ; then
+ ip route add default via $remote_ip table $table
+fi
+logread -f -S 128000 -e dnsmasq |dnsmasq_route -d $dns_server -r $remote_ip -t $table
