406b0810165d807250510773000de5aa32cb9a64
liu shiwei <liushiwei@gmail.com>
2026-06-25 16:59:35 +0800

    使用ipset代替log解析

--- dnsmasq_log.sh
+++
@@ -1,6 +0,0 @@
-$!/bin/sh
-if [ -x logread ] ; then
-logread -l 1000 -e dnsmasq |./dnsmasq_log -V
-else
-tail -n 1000 /var/log/daemon.log |./dnsmasq_log -V
-fi
--- test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat syslog |./dnsmasq_route -s 192.168.0.0/24 -r 10.0.0.2 -d 8.8.4.4 -t 107 -v
