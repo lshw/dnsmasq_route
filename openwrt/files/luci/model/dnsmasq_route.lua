39e641c1487d1c44d698fd19eb766371efba537b
liu shiwei <liushiwei@gmail.com>
2026-06-25 19:00:32 +0800

    调整设置脚本

--- openwrt/files/luci/model/dnsmasq_route.lua
+++ openwrt/files/luci/model/dnsmasq_route.lua
@@ -15,7 +15,6 @@ enable = s:option(Flag, "enabled", "启用")
 
 s:option(Value, "dns_server", "分流dns服务器","下面的分流域名表所列域名，要到上面这个dns服务器进行解析")
 s:option(Value, "remote_ip", "分流路由地址","解析回来的ip为目的地址的流量，将被送到上面这个地址")
-s:option(Flag, "ipv6_disable", "禁用ipv6解析地址","IPV6公网地址，不能修改路由，所以最好禁用").rmempty = false
 
 localnet = s:option(Flag, "localnet_only", "只路由本地网络","只把源自内网LAN的流量，送到分流路由地址")
 localnet.rmempty = false
@@ -32,32 +31,12 @@ end
 function config.write(self, section, value)
 	value = value:gsub("\r\n?", "\n")
 	nixio.fs.writefile("/etc/dnsmasq_route.ini", value)
-        luci.sys.call("/etc/init.d/dnsmasq_route restart >/dev/null;/etc/init.d/dnsmasq restart >/dev/null")
+        luci.sys.call("/etc/init.d/dnsmasq_route start >/dev/null")
 end
 
 function localnet.write(self, section, value)
         Flag.write(self, section, value)
-        luci.sys.call("/etc/init.d/dnsmasq_route restart >/dev/null")
-end
-
-function enable.write(self, section, value)
-        Flag.write(self, section, value)
-        if value == "1" then
-                luci.sys.call("/etc/init.d/dnsmasq_route enable >/dev/null;uci set dhcp.cfg01411c.logqueries='1' >/dev/null;uci commit >/dev/null;/etc/init.d/dnsmasq_route restart >/dev/null")
-        else
-                luci.sys.call("/etc/init.d/dnsmasq_route stop >/dev/null;/etc/init.d/dnsmasq_route disable >/dev/null;uci del dhcp.cfg01411c.logqueries >/dev/null;uci commit >/dev/null;/etc/init.d/dnsmasq reload >/dev/null")
-        end
-end
-
-dnslog = s:option(Value, "dns_log", "未分流dns解析清单", "最近10分钟")
-dnslog.template = "cbi/tvalue"
-dnslog.rows = 10
-dnslog.readonly = 1
-dnslog.wrap = "off"
-
-function dnslog.cfgvalue(self, section)
-	local t = io.popen('logread -l 10000 -e dnsmasq |dnsmasq_log -V -l 600 |sort -r |head -n 200')
-	return t:read("*all")
+        luci.sys.call("/etc/init.d/dnsmasq_route start >/dev/null")
 end
 
 return m
